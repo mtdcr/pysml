657f72bcc10cd5cab8e7b9d28875c9b1812f2bb4
Andreas Oberritter <obi@saftware.de>
2020-11-24 12:46:55 +0100

    setup.py: Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pysml',
-    version='0.0.2',
+    version='0.0.3',
     author='Andreas Oberritter',
     author_email='obi@saftware.de',
     url='https://github.com/mtdcr/pysml',
