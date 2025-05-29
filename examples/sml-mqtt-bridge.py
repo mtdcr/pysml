8dc7b3d7d23b281c51f6c3d25ad34060e40b8eb1
Andreas Oberritter <obi@saftware.de>
2025-05-29 11:52:20 +0200

    Bump copyright year to 2025

--- examples/sml-mqtt-bridge.py
+++ examples/sml-mqtt-bridge.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright 2020 Andreas Oberritter
+# Copyright 2025 Andreas Oberritter
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
